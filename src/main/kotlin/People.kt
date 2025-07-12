9b8393e94221289161bb51f11beb675d8da5e8ea
Wiktor Chumakov <Apostal333@gmail.com>
2025-07-12 13:59:01 +0300

    modified file

--- src/main/kotlin/People.kt
+++ src/main/kotlin/People.kt
@@ -1,13 +1,18 @@
 fun main() {
-    val humans = 1..1_001 step 10
-    val likes = 565
-//
+    val humans = 1..Int.MAX_VALUE step 10
+    val likes = 4141
+
+    val lastTwoDigits = likes % 100  // Последние две цифры (Int)
+    val lastDigit = likes % 10       // Последняя цифра (Int)
+
+    // Проверяем особые случаи
+    val isSpecialCase = lastTwoDigits in 11..14
+
     if (likes !in humans) {
         println("Понравилось $likes людям")
-    } else if (likes == 11  || likes == 111 ){
+    } else if (isSpecialCase) {
         println("Понравилось $likes людям")
-    } else {
+    } else if (lastDigit == 1) {
         println("Понравилось $likes человеку")
     }
-
 }
